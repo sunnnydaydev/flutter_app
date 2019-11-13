aa934b9d2d587c270aef96baff43b55b28ca41d6
sunnydaydev <1246771571@qq.com>
2019-11-13 17:25:08 +0800

    基础控件练习

---
+++ lib/base_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  final Widget title;
+
+  MyAppBar({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.6,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: "Navigation menu",
+            onPressed: null,
+          ),
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: "Search",
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text(
+              "Example title",
+              style: Theme.of(context).primaryTextTheme.title,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text("hello world"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// 基础控件的练习
+void main() => runApp(MaterialApp(
+  title: "My App",
+  home: MyScaffold(),
+));
