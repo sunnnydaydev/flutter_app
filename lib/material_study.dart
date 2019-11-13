c7188d7c76a944881ae1e0b20dda8fd97ecb4436
sunnydaydev <1246771571@qq.com>
2019-11-13 18:44:20 +0800

    控件基础 md

---
+++ lib/material_study.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}
+
+void main() => runApp(MaterialApp(
+      title: "Flutter Tutorial",
+      home: TutorialHome(),
+    ));
