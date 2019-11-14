19d154f338d597d4b7bef1de574661d2d273f4d5
sunnydaydev <1246771571@qq.com>
2019-11-14 14:27:10 +0800

    基础控件add

--- lib/material_study.dart
+++ lib/material_study.dart
@@ -3,12 +3,37 @@ import 'package:flutter/material.dart';
 class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold();
+    return Scaffold(
+      //布局控件
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: "Navigation menu",
+          onPressed: null,
+        ),
+        title: Text("Example title"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: "Search",
+            onPressed: null,
+          )
+        ],
+      ),
+      body: Center(
+        child: Text("hello world"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: "Add",
+         child: Icon(Icons.add),
+      ),
+    );
   }
 }
 
-void main() => runApp(MaterialApp(
+void main() =>
+    runApp(MaterialApp(
       title: "Flutter Tutorial",
       home: TutorialHome(),
     ));
